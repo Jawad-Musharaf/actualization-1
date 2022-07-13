a4b82bbd8aab7cbb5f23f05d6d26e7cd61989441
Jawad-Musharaf <108696004+Jawad-Musharaf@users.noreply.github.com>
2022-07-13 18:03:11 +0500

    updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,51 +1,94 @@
-import React, { useEffect, useState } from "react";
-import { Header, Footer } from "react-fullpage";
+import React, { useEffect, useState, useRef } from "react";
 import "../styles/main.css";
 import OurServices from "./OurServices";
-import Section from "./Section";
-import mouseImage from "../assets/Mouse.svg"
-import logo from "../assets/logo.png"
+import mouseImage from "../assets/Mouse.svg";
+import logo from "../assets/logo.png";
 import Info from "./Info";
+import Contact from "./Contact";
+import About from "./About";
+import { LoaderData } from "./LoaderData";
 
 const Main = () => {
   const [state, setState] = useState({
     scroll: true,
     activeMenu: "Info",
+    isLoading: true,
+    sliderCount: 0,
+    viewCount: 1
   });
 
-  const setMenu = (value) => {
+  const setMenu = (value, index) => {
     setState((pre) => ({
       ...pre,
       activeMenu: value,
+      sliderCount: index
     }));
   };
 
   const activeComponent = {
-    "Info": <Info/>,
+    Info: <Info />,
     "Our Services": <OurServices />,
-    "About Us": <Section content={"About"} />,
-    "Contact": <Section content={"Contact"} />,
+    "About Us": <About />,
+    Contact: <Contact />,
   };
- let pageArray = ["Info", "Our Services", "About Us", "Contact"];
+  let pageArray = ["Info", "Our Services", "About Us", "Contact"];
+
+  const changeSlider = () => {
+    let elementAll = document.querySelectorAll(".blocksection");
+    if (state.sliderCount == 3) {
+      setState(pre => ({ ...pre, activeMenu: pageArray[0], sliderCount: 0 }))
+    }
+    else if (state.sliderCount == 1) {
+      if (elementAll.length == state.viewCount) {
+        elementAll[0].classList.add('active')
+        setState((pre) => ({
+          ...pre,
+          activeMenu: pageArray[state.sliderCount],
+          sliderCount: state.sliderCount++,
+          viewCount: 1
+        }));
+      }
+      else {
+        elementAll[state.viewCount - 1].classList.remove('active')
+        elementAll[state.viewCount].classList.add('active')
+        setState(pre => ({ ...pre, viewCount: state.viewCount++ }))
+      }
+    }
+    else {
+      setState((pre) => ({
+        ...pre,
+        activeMenu: pageArray[state.sliderCount],
+        sliderCount: state.sliderCount++
+      }));
+    }
+  }
+
   useEffect(() => {
     setState((pre) => ({
       ...pre,
       activeMenu: "Info",
     }));
+
+    setTimeout(() => {
+      setState((pre) => ({
+        ...pre,
+        isLoading: false,
+      }));
+    }, 0);
   }, []);
 
-  return (
+  return !state.isLoading ? (
     <>
       <header>
         <div className="logo">
-        <img src={logo}/>
+          <img src={logo} />
         </div>
-    
+
         {pageArray?.map((item, i) => {
           return (
             <span
               key={i}
-              onClick={() => setMenu(item)}
+              onClick={() => setMenu(item, i)}
               className={`${state.activeMenu === item ? "active" : ""}`}
             >
               {item}
@@ -56,9 +99,11 @@ const Main = () => {
 
       <div className="mainContainer">
         {activeComponent[state.activeMenu]}
-        <img src={mouseImage}  className="mousePointer"/>
+        <img src={mouseImage} className="mousePointer" onClick={(() => changeSlider())} />
       </div>
     </>
+  ) : (
+    <LoaderData />
   );
 };
 
