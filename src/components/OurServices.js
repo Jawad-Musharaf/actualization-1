903ae779de06eeade5eacacfbaf2f628a171f259
Jawad-Musharaf <108696004+Jawad-Musharaf@users.noreply.github.com>
2022-07-14 18:43:50 +0500

    updated

--- src/components/OurServices.js
+++ src/components/OurServices.js
@@ -3,6 +3,9 @@ import tablet from "../assets/Tablet.svg";
 import dairy from "../assets/Diary.svg";
 import mobile from "../assets/Mobile.svg";
 import laptop from "../assets/Laptop.svg";
+import glass from "../assets/Magnifying_glass.svg";
+import gear from "../assets/Gear.svg";
+import stats from "../assets/Stats.svg";
 
 
 const OurServices = ({ ref, executeScroll }) => {
@@ -25,7 +28,6 @@ const OurServices = ({ ref, executeScroll }) => {
   const isInViewport2 = useIsInViewport(ref2);
   const isInViewport3 = useIsInViewport(ref3);
 
- 
   function useIsInViewport(ref) {
     const [isIntersecting, setIsIntersecting] = useState(false);
 
@@ -48,118 +50,199 @@ const OurServices = ({ ref, executeScroll }) => {
     return isIntersecting;
   }
 
-  const scrollfun = ()=>{
+  const scrollfun = () => {
     //element.classList.add("active");
-    if(isInViewport1){
-        const element = ref1.current; 
-        element.classList.remove("active")
-       element.classList.add("active")
-     
-    }
-    else if(isInViewport2){
-        const element = ref2.current; 
-        element.classList.remove("active")
-        element.classList.add("active");
-    }
+    // if(isInViewport1){
+    //     const element = ref1.current;
+    //     element.classList.remove("active")
+    //    element.classList.add("active")
+    // }
+    // else if(isInViewport2){
+    //     const element = ref2.current;
+    //     element.classList.remove("active")
+    //     element.classList.add("active");
+    // }
+    // else if(isInViewport3){
+    //     const element = ref3.current;
+    //     element.classList.remove("active")
+    //     element.classList.add("active");
+    // }
+  };
+  let sections = document.getElementsByTagName("section");
+  // tracks index of current section
+  let currentSectionIndex = 0;
 
-    else if(isInViewport3){
-        const element = ref3.current; 
-        element.classList.remove("active")
-        element.classList.add("active");
+  document.addEventListener("wheel", (e) => {
+    if (e.wheelDeltaY > 0 && currentSectionIndex - 1 >= 0) {
+      // wheel up
+      sections[currentSectionIndex].classList.remove("active");
+      currentSectionIndex--;
+      sections[currentSectionIndex].classList.add("active");
+    } else if (e.wheelDeltaY < 0 && currentSectionIndex + 1 < sections.length) {
+      // wheel down
+      sections[currentSectionIndex].classList.remove("active");
+      currentSectionIndex++;
+      sections[currentSectionIndex].classList.add("active");
     }
-    
-  }
+  });
   return (
-    <div className="ourServices" onScroll={()=>scrollfun()}>
+    <div className="ourServices">
       <div className="content">
-        <section className="blocksection page1">
-          <div className="sections" >
-            <div className="animate__animated animate__backInUp">
+        <section className="blocksection page1 active" data-section="0">
+          <div className="sections">
+            <div className="animate__animated animate__fadeInUp">
               <h1 className="maintitle">Our Services</h1>
             </div>
           </div>
         </section>
 
-        <section className="blocksection page2">
-          <div className="sections"  ref={ref1}>
-            <img
-              className="animate__animated animate__zoomIn tabletimg"
-              src={tablet}
-            />
-            <img
-              className="animate__animated animate__zoomIn dairyimg"
-              src={dairy}
-            />
-            <div className="animate__animated animate__zoomIn title1">
-              <h1>Our Services</h1>
-            </div>
-          </div>
-        </section>
-
-        <section className="blocksection page3">
-          <div className="sections" ref={ref2}>
-            <img
-              className="animate__animated animate__zoomIn tabletimg mobileimg"
-              src={mobile}
-            />
-            <div className="animate__animated animate__zoomIn title1 smartimg">
+        <section className="blocksection page2" data-section="1">
+          <div className="container">
+            <div className="sections" ref={ref1}>
               <img
-                className="animate__animated smartimg_Inner"
+                className="animate__animated animate__zoomIn tabletimg"
                 src={tablet}
               />
               <img
-                className="animate__animated smartimg_Inner_b"
+                className="animate__animated animate__zoomIn dairyimg"
                 src={dairy}
               />
-
-              <div className="animate__animated title1">
+              <div className="animate__animated animate__zoomIn title1">
                 <h1>Our Services</h1>
               </div>
             </div>
           </div>
         </section>
 
-        <section className="blocksection page4" >
-          <div className="sections"  ref={ref3}>
-            <img
-              className="animate__animated animate__zoomIn laptopimg"
-              src={laptop}
-            />
-
-            <div className="fade-in-image">
-              <h1>Develop</h1>
-              <p>From Web Apps to Mobile, we develop what your business needs to run better.</p>
-            </div>
-
-            <div className="animate__animated animate__zoomIn smartimg">
-
+        <section className="blocksection page3" data-section="2">
+          <div className="container">
+            <div className="sections" ref={ref2}>
               <img
-                className="animate__animated mobileimg"
+                className="animate__animated animate__zoomIn tabletimg mobileimg"
                 src={mobile}
               />
-            
-              <div className="animate__animated title1 smartimg">
-                
+              <div className="animate__animated animate__zoomIn smartimg">
                 <img
                   className="animate__animated smartimg_Inner"
                   src={tablet}
                 />
+
                 <img
                   className="animate__animated smartimg_Inner_b"
                   src={dairy}
                 />
 
-                {/* <div className="animate__animated title1">
+                <div className="animate__animated smallservice">
                   <h1>Our Services</h1>
-                </div> */}
+                </div>
               </div>
             </div>
+          </div>
+        </section>
 
+        
+        <section className="blocksection page4" data-section="3">
+          <div className="container">
+            <div className="sections" ref={ref3}>
+              <img
+                className="animate__animated animate__zoomIn laptopimg"
+                src={laptop}
+              />
 
-            {/* <img
-              className="animate__animated animate__zoomIn dairyimg"
-              src={dairy}
-            /> */}
+              <div className="fade-in-image">
+                <h1>Develop</h1>
+                <p>
+                  From Web Apps to Mobile, we develop what your business needs to
+                  run better.
+                </p>
+              </div>
+
+              <div className="animate__animated animate__zoomIn laptopimg_inner">
+                <img
+                className="animate__animated tabletimg mobileimg mobileimg_b"
+                src={mobile}
+              />
+                <div className="animate__animated smartimg">
+                  <img
+                    className="animate__animated laptopsmartimg_Inner"
+                    src={tablet}
+                  />
+
+                  <img
+                    className="animate__animated laptopsmartimg_Inner_b"
+                    src={dairy}
+                  />
+
+                  <div className="animate__animated smallservice">
+                    <h1>Our Services</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="blocksection page5" data-section="4">
+          <div className="container">
+            <div className="sections" ref={ref3}>
+              <div className="optimize" ref={ref3}>
+                <img
+                  className="animate__animated animate__zoomIn opt_laptopimg"
+                  src={laptop}
+                />
+
+                <div className="opt_fade-in-image">
+                  <h1>Develop</h1>
+                  <p>
+                    From Web Apps to Mobile, we develop what your business needs to
+                    run better.
+                  </p>
+                </div>
+
+                <div className="animate__animated animate__zoomIn opt_laptopimg_inner">
+                  <img
+                  className="animate__animated tabletimg mobileimg mobileimg_b"
+                  src={mobile}
+                />
+                  <div className="animate__animated smartimg">
+                    <img
+                      className="animate__animated opt_laptopsmartimg_Inner"
+                      src={tablet}
+                    />
+
+                    <img
+                      className="animate__animated opt_laptopsmartimg_Inner_b"
+                      src={dairy}
+                    />
+
+                    <div className="animate__animated smallservice">
+                      <h1>Our Services</h1>
+                    </div>
+                  </div>
+                </div>
+
+                <img
+                  className="animate__animated animate__zoomIn opt_glass"
+                  src={glass}
+                />
+
+                <img
+                  className="animate__animated animate__zoomIn grea_a"
+                  src={gear}
+                />
+
+                <img
+                  className="animate__animated animate__zoomIn grea_b"
+                  src={gear}
+                />
+
+                <img
+                  className="animate__animated animate__zoomIn opt_status"
+                  src={stats}
+                />
+
+              </div>
+            </div>
           </div>
         </section>
       </div>
